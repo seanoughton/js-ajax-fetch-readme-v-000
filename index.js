9a031aa4b9a9f0ea5a3c65bdab95dd6b24769c73
Sean Oughton <seanoughton@gmail.com>
2018-07-06 21:59:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much.";
 
-fetch('https://api.github.com/repos/jquery/jquery/commits')
-  .then(res => res.json())
-  .then(json => console.log(json));
+fetch('https://api.github.com/user/repos').
+  then(res => res.json()).
+  then(json => console.log(json))
